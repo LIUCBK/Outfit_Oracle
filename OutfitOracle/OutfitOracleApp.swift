457b2fc085ecd2223c24e23147f40b28a31e3498
LIUCBK <liucbk@bc.edu>
2025-04-22 22:43:36 -0400

    Initial Commit

---
+++ OutfitOracle/OutfitOracleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutfitOracleApp.swift
+//  OutfitOracle
+//
+//  Created by 刘佳雨 on 2025/4/22.
+//
+
+import SwiftUI
+
+@main
+struct OutfitOracleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
